f9da919c208b7e1e216aaa633443b840bfab4f2a
CKHuangGH <95505429+CKHuangGH@users.noreply.github.com>
2022-02-28 22:36:28 +0100

    new version

--- remote_script/precluster.sh
+++ remote_script/precluster.sh
@@ -2,6 +2,7 @@ for i in {1..20}
 do
 echo "  apiServerAddress: \"$(ifconfig eno1 |grep "inet " | cut -f 10 -d " ")"\" >> /root/mck8s_lsv/remote_script/config_01/kind-example-config-$i.yaml
 echo "  apiServerAddress: \"$(ifconfig eno1 |grep "inet " | cut -f 10 -d " ")"\" >> /root/mck8s_lsv/remote_script/config_02/kind-example-config-$i.yaml
+echo "  apiServerAddress: \"$(ifconfig eno1 |grep "inet " | cut -f 10 -d " ")"\" >> /root/mck8s_lsv/remote_script/config_03/kind-example-config-$i.yaml
 done
 
 ssh-keyscan $1 >> /root/.ssh/known_hosts
