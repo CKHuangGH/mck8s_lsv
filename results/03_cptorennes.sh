216257380eaa833d8f609d8f80f312ea27074e7d
UoFixED <42113547+UoFixED@users.noreply.github.com>
2022-03-29 20:40:47 +0200

    tt

--- results/03_cptorennes.sh
+++ results/03_cptorennes.sh
@@ -1,9 +1,9 @@
-mv kubetopPodD.csv /root/mck8s_lsv/experiment01/results/kubetopPodD.csv
-mv kubetopNode.csv /root/mck8s_lsv/experiment01/results/kubetopNode.csv
-mv kubetopPodKS.csv /root/mck8s_lsv/experiment01/results/kubetopPodKS.csv
-mv kubetopPodM.csv /root/mck8s_lsv/experiment01/results/kubetopPodM.csv
-mv kubetopPodKF.csv /root/mck8s_lsv/experiment01/results/kubetopPodKF.csv
-mv psr.csv /root/mck8s_lsv/experiment01/results/psr.csv
-mv dockerstats.csv /root/mck8s_lsv/experiment01/results/dockerstats.csv
+mv kubetopPodD.csv /root/mck8s_lsv/results/results/kubetopPodD.csv
+mv kubetopNode.csv /root/mck8s_lsv/results/results/kubetopNode.csv
+mv kubetopPodKS.csv /root/mck8s_lsv/results/results/kubetopPodKS.csv
+mv kubetopPodM.csv /root/mck8s_lsv/results/results/kubetopPodM.csv
+mv kubetopPodKF.csv /root/mck8s_lsv/results/results/kubetopPodKF.csv
+mv psr.csv /root/mck8s_lsv/results/results/psr.csv
+mv dockerstats.csv /root/mck8s_lsv/results/results/dockerstats.csv
 
-scp -r /root/mck8s_lsv/experiment01/results chuang@172.16.111.106:/home/chuang/experiment01
+scp -r /root/mck8s_lsv/results/results chuang@172.16.111.106:/home/chuang/results
